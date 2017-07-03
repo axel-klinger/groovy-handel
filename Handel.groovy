7134fcdb2aa9584dfa83677f16312d240c213958
Axel Klinger <axel.klinger@web.de>
2017-07-03 21:21:40 +0200

    Waren und Märkte

--- Handel.groovy
+++ Handel.groovy
@@ -1,9 +1,13 @@
 class Handel {
 
+  static waren = ['Honig', 'Salz', 'Tran', 'Pelze']
+  static marktplaetze = ["Lübeck","Malmö","Riga","Kopenhagen","Bergen"]
+
   static main(args) {
 
     println "Handel"
-
+    println "Waren: " + waren
+    println "Märkte: " + marktplaetze
     // 30 tage handeln
     30.times { number ->
       println "Tag " + (number + 1)
